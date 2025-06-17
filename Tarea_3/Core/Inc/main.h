84e5c41bff5cb23c7a82eac6cb0d75bfbe049c88
Jose Diaz <jodiazgu@unal.edu.co>
2025-06-17 10:17:41 -0500

    AAAA

--- Tarea_3/Core/Inc/main.h
+++ Tarea_3/Core/Inc/main.h
@@ -36,25 +36,7 @@ extern "C" {
 
 /* Exported types ------------------------------------------------------------*/
 /* USER CODE BEGIN ET */
-// Se definen los estados finitos que usaremos para la máquina de estados
-typedef enum{
-	STATE_REFRESH,
-	STATE_RGB_FEEDBACK,
-	STATE_TAXIMETER_FEEDBACK,
-	STATE_CHANGE_REFRESH,
-} State_t;
 
-//Se definen los estados finitos que puede tener el led RGB
-typedef enum{
-	APAGADO,
-	ROJO,
-	VERDE,
-	AZUL,
-	AZUL_VERDE,
-	AZUL_ROJO,
-	ROJO_VERDE,
-	ROJO_VERDE_AZUL,
-}RGB_Color_t ;
 /* USER CODE END ET */
 
 /* Exported constants --------------------------------------------------------*/
@@ -77,61 +59,18 @@ void Error_Handler(void);
 /* Private defines -----------------------------------------------------------*/
 #define B1_Pin GPIO_PIN_13
 #define B1_GPIO_Port GPIOC
-#define B1_EXTI_IRQn EXTI15_10_IRQn
-#define UserLed_Pin GPIO_PIN_1
-#define UserLed_GPIO_Port GPIOH
-#define Switch_Encoder_Pin GPIO_PIN_0
-#define Switch_Encoder_GPIO_Port GPIOC
-#define Switch_Encoder_EXTI_IRQn EXTI0_IRQn
-#define Clk_Encoder_Pin GPIO_PIN_1
-#define Clk_Encoder_GPIO_Port GPIOC
-#define Clk_Encoder_EXTI_IRQn EXTI1_IRQn
 #define USART_TX_Pin GPIO_PIN_2
 #define USART_TX_GPIO_Port GPIOA
 #define USART_RX_Pin GPIO_PIN_3
 #define USART_RX_GPIO_Port GPIOA
-#define BotonTasaRefrescoIncremento_Pin GPIO_PIN_4
-#define BotonTasaRefrescoIncremento_GPIO_Port GPIOA
-#define BotonTasaRefrescoIncremento_EXTI_IRQn EXTI4_IRQn
-#define RGB_VERDE_Pin GPIO_PIN_5
-#define RGB_VERDE_GPIO_Port GPIOA
-#define RGB_AZUL_Pin GPIO_PIN_6
-#define RGB_AZUL_GPIO_Port GPIOA
-#define DigitoD3_Pin GPIO_PIN_5
-#define DigitoD3_GPIO_Port GPIOC
-#define Data_Encoder_Pin GPIO_PIN_2
-#define Data_Encoder_GPIO_Port GPIOB
-#define BotonTasaRefrescoDecremento_Pin GPIO_PIN_15
-#define BotonTasaRefrescoDecremento_GPIO_Port GPIOB
-#define BotonTasaRefrescoDecremento_EXTI_IRQn EXTI15_10_IRQn
-#define DigitoD2_Pin GPIO_PIN_6
-#define DigitoD2_GPIO_Port GPIOC
-#define DigitoD1_Pin GPIO_PIN_8
-#define DigitoD1_GPIO_Port GPIOC
-#define SieteSegmentosLEDA_Pin GPIO_PIN_9
-#define SieteSegmentosLEDA_GPIO_Port GPIOC
-#define SieteSegmentosLEDF_Pin GPIO_PIN_11
-#define SieteSegmentosLEDF_GPIO_Port GPIOA
-#define SieteSegmentosLEDB_Pin GPIO_PIN_12
-#define SieteSegmentosLEDB_GPIO_Port GPIOA
+#define LD2_Pin GPIO_PIN_5
+#define LD2_GPIO_Port GPIOA
 #define TMS_Pin GPIO_PIN_13
 #define TMS_GPIO_Port GPIOA
 #define TCK_Pin GPIO_PIN_14
 #define TCK_GPIO_Port GPIOA
-#define SieteSegmentosLEDE_Pin GPIO_PIN_10
-#define SieteSegmentosLEDE_GPIO_Port GPIOC
-#define SieteSegmentosLEDG_Pin GPIO_PIN_11
-#define SieteSegmentosLEDG_GPIO_Port GPIOC
-#define SieteSegmentosLEDD_Pin GPIO_PIN_12
-#define SieteSegmentosLEDD_GPIO_Port GPIOC
-#define SieteSegmentosLEDC_Pin GPIO_PIN_2
-#define SieteSegmentosLEDC_GPIO_Port GPIOD
 #define SWO_Pin GPIO_PIN_3
 #define SWO_GPIO_Port GPIOB
-#define DigitoD0_Pin GPIO_PIN_7
-#define DigitoD0_GPIO_Port GPIOB
-#define RGB_ROJO_Pin GPIO_PIN_8
-#define RGB_ROJO_GPIO_Port GPIOB
 
 /* USER CODE BEGIN Private defines */
 
--- Tarea_3/Core/Src/stm32f4xx_it.c
+++ Tarea_3/Core/Src/stm32f4xx_it.c
@@ -55,8 +55,7 @@
 /* USER CODE END 0 */
 
 /* External variables --------------------------------------------------------*/
-extern TIM_HandleTypeDef htim2;
-extern TIM_HandleTypeDef htim3;
+
 /* USER CODE BEGIN EV */
 
 /* USER CODE END EV */
@@ -199,90 +198,6 @@ void SysTick_Handler(void)
 /* please refer to the startup file (startup_stm32f4xx.s).                    */
 /******************************************************************************/
 
-/**
-  * @brief This function handles EXTI line0 interrupt.
-  */
-void EXTI0_IRQHandler(void)
-{
-  /* USER CODE BEGIN EXTI0_IRQn 0 */
-
-  /* USER CODE END EXTI0_IRQn 0 */
-  HAL_GPIO_EXTI_IRQHandler(Switch_Encoder_Pin);
-  /* USER CODE BEGIN EXTI0_IRQn 1 */
-
-  /* USER CODE END EXTI0_IRQn 1 */
-}
-
-/**
-  * @brief This function handles EXTI line1 interrupt.
-  */
-void EXTI1_IRQHandler(void)
-{
-  /* USER CODE BEGIN EXTI1_IRQn 0 */
-
-  /* USER CODE END EXTI1_IRQn 0 */
-  HAL_GPIO_EXTI_IRQHandler(Clk_Encoder_Pin);
-  /* USER CODE BEGIN EXTI1_IRQn 1 */
-
-  /* USER CODE END EXTI1_IRQn 1 */
-}
-
-/**
-  * @brief This function handles EXTI line4 interrupt.
-  */
-void EXTI4_IRQHandler(void)
-{
-  /* USER CODE BEGIN EXTI4_IRQn 0 */
-
-  /* USER CODE END EXTI4_IRQn 0 */
-  HAL_GPIO_EXTI_IRQHandler(BotonTasaRefrescoIncremento_Pin);
-  /* USER CODE BEGIN EXTI4_IRQn 1 */
-
-  /* USER CODE END EXTI4_IRQn 1 */
-}
-
-/**
-  * @brief This function handles TIM2 global interrupt.
-  */
-void TIM2_IRQHandler(void)
-{
-  /* USER CODE BEGIN TIM2_IRQn 0 */
-
-  /* USER CODE END TIM2_IRQn 0 */
-  HAL_TIM_IRQHandler(&htim2);
-  /* USER CODE BEGIN TIM2_IRQn 1 */
-
-  /* USER CODE END TIM2_IRQn 1 */
-}
-
-/**
-  * @brief This function handles TIM3 global interrupt.
-  */
-void TIM3_IRQHandler(void)
-{
-  /* USER CODE BEGIN TIM3_IRQn 0 */
-
-  /* USER CODE END TIM3_IRQn 0 */
-  HAL_TIM_IRQHandler(&htim3);
-  /* USER CODE BEGIN TIM3_IRQn 1 */
-
-  /* USER CODE END TIM3_IRQn 1 */
-}
-
-/**
-  * @brief This function handles EXTI line[15:10] interrupts.
-  */
-void EXTI15_10_IRQHandler(void)
-{
-  /* USER CODE BEGIN EXTI15_10_IRQn 0 */
-
-  /* USER CODE END EXTI15_10_IRQn 0 */
-  HAL_GPIO_EXTI_IRQHandler(B1_Pin);
-  HAL_GPIO_EXTI_IRQHandler(BotonTasaRefrescoDecremento_Pin);
-  /* USER CODE BEGIN EXTI15_10_IRQn 1 */
-
-  /* USER CODE END EXTI15_10_IRQn 1 */
-}
-
 /* USER CODE BEGIN 1 */
+
 /* USER CODE END 1 */
